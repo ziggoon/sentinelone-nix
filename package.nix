3715b812b665d4dae716d0178baaef44cdfc3d0d
ziggoon <79215783+ziggoon@users.noreply.github.com>
2025-02-03 13:54:40 -0600

    Update package.nix

--- package.nix
+++ package.nix
@@ -15,20 +15,12 @@ in
 stdenv.mkDerivation {
   pname = "sentinelone";
   version = "24.3.3.1";
-
-  src = fetchurl {
-    url = "https://imugit.imubit.com/morgan.helton/sentinelone/-/raw/main/${sentinelOnePackage}";
-    hash = "sha256-EgahRYXm3eceaDnR8wf6qQ6kirk1xC+epbHjj1KyLlc=";
-  };
-
+  src = ./. + "/${sentinelOnePackage}";  # Changed to use local file
   unpackPhase = ''
     runHook preUnpack
-
     dpkg-deb -x $src .
-
     runHook postUnpack
   '';
-
   nativeBuildInputs = [
     dpkg
     autoPatchelfHook
@@ -38,20 +30,16 @@ stdenv.mkDerivation {
     jq
     gcc-unwrapped
   ];
-
   installPhase = ''
     mkdir -p $out/opt/
     mkdir -p $out/cfg/
     mkdir -p $out/bin/
-
     cp -r opt/* $out/opt
-
     ln -s $out/opt/sentinelone/bin/sentinelctl $out/bin/sentinelctl
     ln -s $out/opt/sentinelone/bin/sentinelone-agent $out/bin/sentinelone-agent
     ln -s $out/opt/sentinelone/bin/sentinelone-watchdog $out/bin/sentinelone-watchdog
     ln -s $out/opt/sentinelone/lib $out/lib
   '';
-
   preFixup = ''
     patchelf --replace-needed libelf.so.0 libelf.so $out/opt/sentinelone/lib/libbpf.so
   '';
